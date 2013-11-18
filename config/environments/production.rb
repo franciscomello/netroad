ec4ac7fdb305a611581f646bdb4d2cedd66118a2
franciscomello <franciscomello@hotmail.com>
2013-11-18 02:13:12 +0000

    devise installed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Netroad::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { :host => 'http://roadshowz-commits.herokuapp.com/' }
 
   # Code is not reloaded between requests.
   config.cache_classes = true

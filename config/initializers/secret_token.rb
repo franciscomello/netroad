e192dbc13915314701726f52635c3005b2c6c9a0
franciscomello <franciscomello@hotmail.com>
2013-11-18 00:37:33 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Netroad::Application.config.secret_key_base = '0ccccee30af8be21730dc64627affb1aea2621dfc16e5fe04a04ef23b984da6fbe1a612ea4f990007400376896c54a12f33ffafeeac01c1cace3852186522837'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Netroad::Application.config.session_store :cookie_store, key: '_netroad_session'
